afa94234102587855ba23620073912703b72e079
OneOneTwoOne <hcburn@gmail.com>
2017-03-03 22:13:50 +0000

    all

--- mobile/src/main/java/com/oneonetwoone/yass/Bullet.java
+++ mobile/src/main/java/com/oneonetwoone/yass/Bullet.java
@@ -1,8 +1,36 @@
 package com.oneonetwoone.yass;
 
-/**
- * Created by Home on 03/03/2017.
- */
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-public class Bullet {
+import com.oneonetwoone.yass.engine.GameObject;
+
+
+
+public class Bullet extends GameObject {
+
+    public Bullet(View view, double pixelFactor){
+        Context c = view.getContext();
+
+        mSpeedFactor= pixelFactor*-300d/1000d;
+
+        mImageView= new ImageView(c);
+        Drawable bulletDrawable=c.getResources().getDrawable(R.drawable.bullet);
+
+        mImageHeight= bulletDrawable.getIntrinsicHeight()*pixelFactor;
+        mImageWidth= bulletDrawable.getIntrinsicWidth()*pixelFactor;
+
+        mImageView.setLayoutParams(new ViewGroup.LayoutParams((int) (mImageWidth),(int) (imageHeight)));
+        mImageView.setImageDrawable(bulletDrawable);
+        mImageView.setVisibility(View.GONE);
+        ((FrameLayout) view).addView(mImageView);
+    }
+
+    public init(){}
+
+
 }
--- mobile/src/main/java/com/oneonetwoone/yass/Player.java
+++ mobile/src/main/java/com/oneonetwoone/yass/Player.java
@@ -11,28 +11,36 @@ import android.widget.TextView;
 import com.oneonetwoone.yass.engine.GameEngine;
 import com.oneonetwoone.yass.engine.GameObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player extends GameObject {
     public double mPositionX,mPositionY, mMaxX, mMaxY;
     public double mSpeedFactor;
     public TextView mTextView;
     public ImageView mShip;
+    public List<Bullet> mBullets;
+    public int INITIAL_BULLET_POOL_AMOUNT=0;
+    public long TIME_BETWEEN_BULLETS;
+    double mPixelFactor;
+    long mTimeSinceLastFire;
 
     public Player(final View view){
-
-        double pixelFactor= view.getHeight() /400d;
-        mSpeedFactor=pixelFactor*100d/1000d;
+        mBullets = new ArrayList<>();
+        mPixelFactor= view.getHeight() /400d;
+        mSpeedFactor=mPixelFactor*100d/1000d;
         mMaxX=view.getWidth()-view.getPaddingRight()-view.getPaddingLeft();
         mMaxY=view.getHeight() - view.getPaddingTop()-view.getPaddingBottom();
         mTextView=(TextView) view.findViewById(R.id.player);
         mShip= new ImageView(view.getContext());
         Drawable shipDrawable = view.getContext().getResources().getDrawable(R.drawable.ship);
         mShip.setLayoutParams(new ViewGroup.LayoutParams(
-                (int) (shipDrawable.getIntrinsicWidth()*pixelFactor),
-                (int) (shipDrawable.getIntrinsicHeight()*pixelFactor)));
+                (int) (shipDrawable.getIntrinsicWidth()*mPixelFactor),
+                (int) (shipDrawable.getIntrinsicHeight()*mPixelFactor)));
         mShip.setImageDrawable(shipDrawable);
 
-        mMaxX -= (shipDrawable.getIntrinsicWidth()*pixelFactor);
-        mMaxY -= (shipDrawable.getIntrinsicHeight()*pixelFactor);
+        mMaxX -= (shipDrawable.getIntrinsicWidth()*mPixelFactor);
+        mMaxY -= (shipDrawable.getIntrinsicHeight()*mPixelFactor);
 
         ((FrameLayout) view).addView(mShip);
     }
@@ -45,6 +53,8 @@ public class Player extends GameObject {
 
     @Override
     public void onUpdate(long elapsedMillis, GameEngine gameEngine){
+        updatePosition(elapsedMillis, gameEngine.mInputController);
+        checkFiring(elapsedMillis, gameEngine);
         InputController inputController = gameEngine.mInputController;
         mPositionX +=
                 mSpeedFactor*inputController.mHorizontalFactor*elapsedMillis;
@@ -64,10 +74,44 @@ public class Player extends GameObject {
         }
     }
 
+    private void checkFiring(long elapsedMillis, GameEngine gameEngine){
+        if (gameEngine.mInputController.mIsFiring && mTimeSinceLastFire > TIME_BETWEEN_BULLETS){
+            Bullet b = getBullet();
+            if (b == null){
+                return;
+            }
+            b.init(mPositionX + mShip.getWidth()/2, mPositionY);
+            gameEngine.addGameObject(b);
+            mTimeSinceLastFire = 0;
+        }
+        else{
+            mTimeSinceLastFire+=elapsedMillis;
+        }
+    }
+
+    public void updatePosition(){}
+
     @Override
     public void onDraw(){
         mTextView.setText("["+ mPositionX +","+mPositionY+"]");
         mShip.setTranslationX((int) mPositionX);
         mShip.setTranslationY((int) mPositionY);
+    }
+
+    private void initBulletPool(){
+        for (int i=0; i<INITIAL_BULLET_POOL_AMOUNT; i++){
+            mBullets.add(new Bullet(mPixelFactor));
+        }
+    }
+
+    private Bullet getBullet(){
+        if (mBullets.isEmpty()){
+            return null;
+        }
+        return mBullets.remove(0);
+    }
+
+    private void releaseBullet(Bullet b){
+        mBullets.add(b);
     }
 }
