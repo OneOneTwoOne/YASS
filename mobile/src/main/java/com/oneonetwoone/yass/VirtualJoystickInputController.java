c516e240f49200fb3028155594bbad0f6b618986
OneOneTwoOne <hcburn@gmail.com>
2017-03-06 22:21:29 +0000

    what

--- mobile/src/main/java/com/oneonetwoone/yass/VirtualJoystickInputController.java
+++ mobile/src/main/java/com/oneonetwoone/yass/VirtualJoystickInputController.java
@@ -1,10 +1,7 @@
 package com.oneonetwoone.yass;
 
+import android.view.MotionEvent;
 import android.view.View;
-
-/**
- * Created by Hugh on 06/03/2017.
- */
 
 public class VirtualJoystickInputController extends InputController {
     public double mMaxDistance;
@@ -16,4 +13,49 @@ public class VirtualJoystickInputController extends InputController {
         double pixelFactor = view.getHeight()/400d;
         mMaxDistance=50*pixelFactor;
     }
+    private class VJoystickTouchListener implements View.OnTouchListener {
+
+        public double mStartingPositionX,mStartingPositionY;
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getActionMasked();
+            if (action == MotionEvent.ACTION_DOWN) {
+                mStartingPositionX = event.getX(0);
+                mStartingPositionY = event.getY(0);
+            } else if (action == MotionEvent.ACTION_UP) {
+                mHorizontalFactor = 0;
+                mVerticalFactor = 0;
+            } else if (action == MotionEvent.ACTION_MOVE) {
+                mHorizontalFactor = (event.getX(0) - mStartingPositionX) / mMaxDistance;
+                if (mHorizontalFactor > 1) {
+                    mHorizontalFactor = 1;
+                } else if (mHorizontalFactor < -1) {
+                    mHorizontalFactor = -1;
+                }
+                mVerticalFactor = (event.getY(0)-mStartingPositionY)/mMaxDistance;
+                if (mVerticalFactor < -1) {
+                    mVerticalFactor = -1;
+                }
+                else if (mVerticalFactor < -1){
+                    mVerticalFactor=-1;
+                }
+            }
+            return true;
+        }
+    }
+    private class VFireButtonTouchListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event){
+            int action = event.getActionMasked();
+            if (action == MotionEvent.ACTION_DOWN){
+                mIsFiring = true;
+            }
+            else if (action == MotionEvent.ACTION_UP){
+                mIsFiring = false;
+            }
+            return true;
+        }
+    }
+
+
 }
